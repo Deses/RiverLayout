582afd12e1b31485fc273e7ac51f0d01f7775ee8
Carlos R <deses12@gmail.com>
2015-10-01 11:06:07 +0000

    Maven artifacts for 1.1

--- src/main/java/se/datadosen/util/JComponentHolder.java
+++
@@ -1,9 +0,0 @@
-package se.datadosen.util;
-
-/**
- * Classes implementing this interface indicate that they contain JComponents
- * that are to be automatically synchronized with a bean or Property object
- * using either the BeanBinder or PropertyBinder class
- */
-public interface JComponentHolder {
-}
